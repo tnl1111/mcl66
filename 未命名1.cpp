2848677d91a29fe3d23566c5eae5f9bbd3fd6e11
tnl1111 <3296714096@qq.com>
2021-02-06 22:47:50 -0600

    added a file

---
+++ 未命名1.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+int main()
+{
+	int temp[5] ={1,3,5,7,9};
+	int (*p2)[5] = &temp;
+	int i;
+	
+	for(i=0; i<5; i++)
+	{
+		
+	    printf("%d\n",*p2+i);
+	    printf("%d\n",*(*p2)+i);
+	    printf("%d\n",*(*p2+i));
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+    }
+	return 0;
+ } 
